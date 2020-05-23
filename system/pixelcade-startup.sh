213dae5fa9ba0f4e96dc53f46d72d5e0768f8693
Alfred Linke <alinke@yahoo.com>
2020-05-23 14:50:42 -0700

    cleaned up openbor artwork

--- system/pixelcade-startup.sh
+++ system/pixelcade-startup.sh
@@ -20,7 +20,7 @@ while [  $STATE == "error" ]; do
     if [[ -f "$HOME/pixelcade/deletemeafterwificonnect.txt" ]]; then  #still exists so we are not wifi complete so show a different message
         curl  "http://localhost:8080/text?t=Using%20your%20phone%20or%20computer,%20connect%20to%20the%20%22Pixelcade%20Setup%22%20WiFi%20and%20then%20navigate%20to%20http%20:%20//%2010%20.%200%20.%200%20.%201%20%20from%20your%20device%20web%20browser%20.%20%20Then%20select%20your%20normal%20WiFi%20network%20and%20password%20.&size=32&loop=10&color=cyan&font=Tall%20Films%20Fine&yoffset=-2"
     else
-        curl  "http://localhost:8080/text?t=wifi%20not%20connected%20troubleshooting%20guide%20at%20pixelcade.org/pi-install&size=14&l=1&color=red"
+        curl  "http://localhost:8080/text?t=wifi%20not%20connected%20troubleshooting%20guide%20at%20pixelcade.org/pi-install&size=14&l=5&color=red"
     fi
     #sleep for 2 seconds and try again
     sleep 2
