2944530a011ad973e98042be44ec2ad7a5830db9
Alfred Linke <alinke@yahoo.com>
2020-05-23 21:56:10 -0700

    taito updates

--- system/update.sh
+++ system/update.sh
@@ -14,10 +14,12 @@ echo "${magenta}Checking for Updates...${white}"
 git stash #stash away your local changes
 git pull  #pull the latest
 if git stash pop | grep -q 'conflict'; then           #put back your local changes and if there is a conflict , let's keep your changes
+  cd $HOME/pixelcade
   echo "${yellow}Auto resolving conflict(s) and keeping your version(s) of the conflicting file(s)...${white}"
   git checkout --theirs *                             #change theirs to ours if we want to keep the pixelcade version instead
   git add *
   git commit -m "Pixelcade Update $today"
+  git ls-files --unmerged | perl -n -e'/\t(.*)/ && print "$1\n"' | uniq | xargs -r git checkout --theirs 
 else
   echo "${yellow}Update complete with no conflicts${white}"
 fi
