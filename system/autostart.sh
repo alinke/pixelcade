634c787b07fd83d701616a5207adf5f0a394a4d4
Alfred Linke <alinke@yahoo.com>
2020-06-26 00:49:16 -0700

    switching autostart.sh over to pixelweb app image

--- system/autostart.sh
+++ system/autostart.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # startup script for a dedicated Pi for Pixelcade, systemd calls this via /etc/systemd/system/pixelcade.service
 
-cd /home/pi/pixelcade && java -jar pixelweb.jar -b &
+cd /home/pi/pixelcade && ./pixelweb -b &
 sleep 10 && cd /home/pi/pixelcade/system && ./pixelcade-startup.sh
