35269390959a05adf1f74b9e1630e154a69fef00
Alfred Linke <al@ledpixelart.com>
2021-10-17 00:19:43 -0700

    got rid of the scrolling ip address text which many users didn't like and changed to show pixelcade logo on startup

--- system/autostart.sh
+++ system/autostart.sh
@@ -2,4 +2,5 @@
 # startup script for a dedicated Pi for Pixelcade, systemd calls this via /etc/systemd/system/pixelcade.service
 
 cd /home/pi/pixelcade && java -jar pixelweb.jar -b &
-sleep 10 && cd /home/pi/pixelcade/system && ./pixelcade-startup.sh
+sleep 10 && curl localhost:8080/arcade/stream/mame/pixelcade
+#sleep 10 && cd /home/pi/pixelcade/system && ./pixelcade-startup.sh
