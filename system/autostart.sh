412aa58a8152e6bf4a87619fdce8779177724ec6
Alfred Linke <al@ledpixelart.com>
2022-02-18 21:20:23 -0800

    adding silent mode to autostart

--- system/autostart.sh
+++ system/autostart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # startup script for a dedicated Pi for Pixelcade, systemd calls this via /etc/systemd/system/pixelcade.service
 
-cd /home/pi/pixelcade && java -jar pixelweb.jar -b &
+cd /home/pi/pixelcade && java -jar pixelweb.jar -b -s &
 sleep 10 && curl localhost:8080/arcade/stream/mame/pixelcade
 #sleep 10 && cd /home/pi/pixelcade/system && ./pixelcade-startup.sh
